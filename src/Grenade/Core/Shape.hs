c1caeaded70925a60a8ec4fc8f538f36ac69dca8
koenigmaximilian <github@koenigmaximilian.de>
2018-07-08 00:39:01 +0000

    Update Shape.hs

--- src/Grenade/Core/Shape.hs
+++ src/Grenade/Core/Shape.hs
@@ -35,7 +35,7 @@ import           Data.Singletons.TypeLits
 import           Data.Vector.Storable ( Vector )
 import qualified Data.Vector.Storable as V
 
-import           GHC.TypeNats ((*))
+import           GHC.TypeNats (type (*))
 
 import qualified Numeric.LinearAlgebra.Static as H
 import           Numeric.LinearAlgebra.Static
